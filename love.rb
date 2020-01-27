9b27d29ab3d82eed9f1a7c1aaa65eeb4ed74d438
Pestenariu Claudiu Andrei <47329003+Swishyfishie@users.noreply.github.com>
2020-01-27 17:37:43 +0000

    Add files via upload

---
+++ love.rb
@@ -0,0 +1,8 @@
+def andrei_loves
+    puts "I hope that someone reads this in the year 4020."
+    puts "I love Cora, my mom."
+    puts "I love Doru, my best friend."
+    puts "I love Mara, my cat."
+end
+
+andrei_loves
